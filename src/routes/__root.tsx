a851b51c4e689d465d09aa1a8ea8ba1c7268a50a
MiniKerumi <74300239+MiniKerumi@users.noreply.github.com>
2026-09-08 08:47:05 +0800

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -123,8 +123,9 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
-      <Outlet />
+      <PanelTransition>
+        <Outlet />
+      </PanelTransition>
     </QueryClientProvider>
   );
 }
